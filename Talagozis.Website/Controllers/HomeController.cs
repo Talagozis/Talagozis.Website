787509bc60e8a5879acc8b27d0e6535d48ef20ba
Talagozis Christos <talagozis8@gmail.com>
2017-07-30 22:38:17 +0300

    Add template. (#1)
    * Add template.
    
    * Create README.md

--- Talagozis.Website/Controllers/HomeController.cs
+++ Talagozis.Website/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Talagozis.Website.App_Plugins;
 
 namespace Talagozis.Website.Controllers
 {
@@ -10,26 +11,30 @@ namespace Talagozis.Website.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return this.CV();
         }
 
-        public IActionResult About()
+        public IActionResult CV()
         {
-            ViewData["Message"] = "Your application description page.";
+            Person person = new Person
+            {
+                forename = "Christos",
+                surname = "Talagozis",
+                dob = new DateTime(1989, 10, 10),
+                pob = "Serres, Greece",
+                email = "christos@talagozis.com",
+                gender = "Male",
+                jobTitles = new string[] { "Backend Web Developer", "Desktop Developer" },
+                linkedinLink = "https://www.linkedin.com/in/talagozis/"
+        };
 
-            return View();
-        }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            return View("Views/Home/CV.cshtml", person);
         }
 
         public IActionResult Error()
         {
-            return View();
+            return View("Views/Shared/Error.cshtml");
         }
     }
 }
