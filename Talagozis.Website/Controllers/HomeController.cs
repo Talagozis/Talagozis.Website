21301c9f2f8dd2b561d5b18be5121b83cb867dd2
Talagozis <talagozis8@gmail.com>
2017-07-31 20:48:46 +0300

    Add Education and WorkingExperience.

--- Talagozis.Website/Controllers/HomeController.cs
+++ Talagozis.Website/Controllers/HomeController.cs
@@ -15,6 +15,9 @@ namespace Talagozis.Website.Controllers
 
         public IActionResult CV()
         {
+            string TEST_TEXT = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
+
             Person person = new Person
             {
                 forename = "Christos",
@@ -24,7 +27,59 @@ namespace Talagozis.Website.Controllers
                 email = "christos@talagozis.com",
                 gender = "Male",
                 jobTitles = new string[] { "Backend Web Developer", "Desktop Developer" },
-                linkedinLink = "https://www.linkedin.com/in/talagozis/"
+                linkedinLink = "https://www.linkedin.com/in/talagozis/",
+                educations = new List<Education>
+                {
+                    new Education
+                    {
+                        title = "Master of Science in Applied Informatics",
+                        courseYears = 2,
+                        description = TEST_TEXT,
+                        graduationDate = new DateTime(2017, 5, 1),
+                        university = "Technological Educational Institute of Central Macedonia, Greece",
+                    },
+                    new Education
+                    {
+                        title = "Bachelor degree in Informatics Engineering",
+                        courseYears = 4,
+                        description = TEST_TEXT,
+                        graduationDate = new DateTime(2014, 9, 1),
+                        university = "Technological Educational Institute of Central Macedonia, Greece",
+                    },
+                },
+                workingExperience = new List<WorkingExperience>
+                {
+                    new WorkingExperience
+                    {
+                        position = "Full-Stack Software Developer",
+                        companyName = "Freelancer",
+                        startDate = new DateTime(2014, 10, 1),
+                        endDate = null,
+                        description = "Design, develop, test, publish and monitor desktop and web applications in .NET",
+                        project = "",
+                        typeOfBusiness = "Software development for business automation"
+                    },
+                    new WorkingExperience
+                    {
+                        position = "Full-Stack Web Developer",
+                        companyName = "BlumenRiviera",
+                        startDate = new DateTime(2013, 10, 1),
+                        endDate = new DateTime(2014, 8, 1),
+                        description = "Design, develop and test web applications in Java, Velocity and PHP",
+                        project = "",
+                        typeOfBusiness = "Software development for vacation booking",
+                    },
+                    new WorkingExperience
+                    {
+                        position = "Desktop Developer",
+                        companyName = "Serres Computer Software",
+                        startDate = new DateTime(2012, 11, 1),
+                        endDate = new DateTime(2013, 5, 1),
+                        description = "Design and develop software with .NET, Test software, Publish and reporting",
+                        project = "",
+                        typeOfBusiness = "",
+                    },
+                },
         };
 
 
--- Talagozis.Website/Models/WorkingExperience.cs
+++ Talagozis.Website/Models/WorkingExperience.cs
@@ -4,11 +4,12 @@ namespace Talagozis.Website.Models
 {
     public class WorkingExperience
     {
-        public string title { get; set; }
+        public string position { get; set; }
         public string companyName { get; set; }
         public string project { get; set; }
         public DateTime startDate { get; set; }
         public DateTime? endDate { get; set; }
         public string description { get; set; }
+        public string typeOfBusiness { get; set; }
     }
 }
