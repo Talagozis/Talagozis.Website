f526e60182f29187250bb4b5bf7659bf26e4e478
Christos Talagozis <talagozis@hotmail.com>
2018-09-16 22:04:53 +0300

    Style articles in Archive page.

--- Talagozis.Website/Models/BlogArchive.cs
+++ Talagozis.Website/Models/BlogArchive.cs
@@ -6,5 +6,10 @@ namespace Talagozis.Website.Models
     [PageType(Title = "Blog archive", UseBlocks = false)]
     public class BlogArchive  : BlogPage<BlogArchive>
     {
+        /// <summary>
+        /// Gets/sets the heading.
+        /// </summary>
+        [Region]
+        public Regions.Heading Heading { get; set; }
     }
 }
