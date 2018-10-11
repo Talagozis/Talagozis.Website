7058770fe6c1c44d62a63e58ffa5c48c7ac67caf
Christos Talagozis <talagozis@hotmail.com>
2018-10-11 07:25:31 +0300

    Fix blog link in HomePage. Removed Montserrat font.

--- Talagozis.Website/Controllers/HomeController.cs
+++ Talagozis.Website/Controllers/HomeController.cs
@@ -52,18 +52,26 @@ namespace Talagozis.Website.Controllers
         {
             var csharpArchiveId = new Guid("f6682da4-11f4-40b4-b118-470bcc198613");
             var javaArchiveId = new Guid("e8ed04db-e33b-46fe-97d7-e0e025a269e2");
+            var machineLearningArchiveId = new Guid("f1891e6d-1efc-4795-8759-133bb977c9e4");
+            var ionicArchiveId = new Guid("d3f5e1a5-7f27-4fe9-b033-4a817745694d");
+            var flutterArchiveId = new Guid("09b2c5b9-21ac-4ffe-8413-5bc0256b7756");
+            var ReactJsArchiveId = new Guid("6e23dca6-c9ae-4560-af54-ffffea6edefd");
 
             // var csharpPosts = _api.Posts.GetAll<Models.BlogPost>(csharpArchiveId).ToList();
             // var javaPosts = _api.Posts.GetAll<Models.BlogPost>(javaArchiveId).ToList();
 			var archives = new List<Models.BlogArchive>
             { 
                 _api.Archives.GetById<Models.BlogArchive>(csharpArchiveId, 1, null, null, null), 
-                _api.Archives.GetById<Models.BlogArchive>(javaArchiveId, 1, null, null, null), 
+                _api.Archives.GetById<Models.BlogArchive>(javaArchiveId, 1, null, null, null),
+                _api.Archives.GetById<Models.BlogArchive>(machineLearningArchiveId, 1, null, null, null),
+                _api.Archives.GetById<Models.BlogArchive>(ionicArchiveId, 1, null, null, null),
+                _api.Archives.GetById<Models.BlogArchive>(flutterArchiveId, 1, null, null, null),
+                _api.Archives.GetById<Models.BlogArchive>(ReactJsArchiveId, 1, null, null, null),
+
             };
 
 			// ViewBag.latestPosts = csharpPosts.Concat(javaPosts);
 			ViewBag.archives = archives;
-
 
 			return View("~/Views/Home/Blog.cshtml", archives);
         }
