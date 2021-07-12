94be4d87adf114fdc1ce30bfe4a5e5487398a557
Talagozis <talagozis8@gmail.com>
2021-07-12 11:30:09 +0300

    Add draft optional parameter to CmsController.

--- Talagozis.Website/Controllers/CmsController.cs
+++ Talagozis.Website/Controllers/CmsController.cs
@@ -44,12 +44,13 @@ namespace Talagozis.Website.Controllers
         /// <param name="page">The optional page</param>
         /// <param name="category">The optional category</param>
         /// <param name="tag">The optional tag</param>
+        /// <param name="draft">The optional draft</param>
         [Route("archive")]
-        public async Task<IActionResult> Archive(Guid id, int? year = null, int? month = null, int? page = null, Guid? category = null, Guid? tag = null)
+        public async Task<IActionResult> Archive(Guid id, int? year = null, int? month = null, int? page = null, Guid? category = null, Guid? tag = null, bool? draft = null)
         {
             BlogArchive blogArchive = await this._api.Pages.GetByIdAsync<BlogArchive>(id);
 
-            if (!blogArchive.IsPublished)
+            if (!blogArchive.IsPublished && draft != true)
                 return this.NotFound();
 
             PostArchive<BlogPost> postArchive = await this._api.Archives.GetByIdAsync<BlogPost>(id, page, category, tag, year, month, pageSize: 20);
@@ -68,12 +69,13 @@ namespace Talagozis.Website.Controllers
         /// Gets the post with the given id.
         /// </summary>
         /// <param name="id">The unique post id</param>
+        /// <param name="draft">The optional draft</param>
         [Route("post")]
-        public async Task<IActionResult> Post(Guid id)
+        public async Task<IActionResult> Post(Guid id, bool? draft = null)
         {
             BlogPost blogPost = await this._api.Posts.GetByIdAsync<BlogPost>(id);
 
-            if (!blogPost.IsPublished)
+            if (!blogPost.IsPublished && draft != true)
                 return this.NotFound();
 
             BlogArchive blogArchive = await this._api.Pages.GetByIdAsync<BlogArchive>(blogPost.BlogId);
@@ -105,12 +107,16 @@ namespace Talagozis.Website.Controllers
         /// Gets the page with the given id.
         /// </summary>
         /// <param name="id">The unique page id</param>
+        /// <param name="draft">The optional draft</param>
         [Route("page")]
-        public async Task<IActionResult> Page(Guid id) 
+        public async Task<IActionResult> Page(Guid id, bool? draft = null) 
         {
-            var model = await this._api.Pages.GetByIdAsync<StandardPage>(id);
+            StandardPage standardPage = await this._api.Pages.GetByIdAsync<StandardPage>(id);
 
-            return this.View(model);
+            if (!standardPage.IsPublished && draft != true)
+                return this.NotFound();
+
+            return this.View(standardPage);
         }
 
     }
